0deb6729706e3e873eb8b255360aa1e86e1cfb1b
Shirozo <caranzoneilbryan@gmail.com>
2024-02-14 23:59:20 +0800

    Added a main function

--- model/training.py
+++ model/training.py
@@ -28,34 +28,36 @@ def split_dataset(dataset, split=.9):
 
 def train_hmm(dataset):
     trainer = hmm.HiddenMarkovModelTrainer()
-    trainer.train(dataset)
     for _ in range(500):
         tagger = trainer.train_supervised(dataset)
     return tagger
 
+def main():
+    files = os.listdir("../Tagged_Article")
+    testing = []
+    for i in  range(len(files)-25):
+    #open the dataset
+        print(f"Traning: {files[i]}")
+        train_data = load_dataset_from_csv(f"../Tagged_Article/{files[i]}")
+        #Split the dataset
+        train, test = split_dataset(train_data)
+        testing.append(test[0])
+        # #Train
+        tagger = train_hmm(train)
 
-files = os.listdir("../Tagged_Article")
-testing = []
-for i in  range(len(files)-25):
-#open the dataset
-    print(f"Traning: {files[i]}")
-    train_data = load_dataset_from_csv(f"../Tagged_Article/{files[i]}")
-    #Split the dataset
-    train, test = split_dataset(train_data)
-    testing.append(test[0])
-    # #Train
-    tagger = train_hmm(train)
+        # #Accuracy tester
+    acurracy = tagger.accuracy(testing)
+    print(f"Accuracy: {acurracy}")
 
-    # #Accuracy tester
-acurracy = tagger.accuracy(testing)
-print(f"Accuracy: {acurracy}")
+    # #Test
+    # sentence = "usa, duha"
+    # tokens = nltk.word_tokenize(sentence.lower())
+    # tagged_sentence = tagger.tag(tokens)
+    # print("Tagged sentence:", tagged_sentence)
 
-# #Test
-# sentence = "usa, duha"
-# tokens = nltk.word_tokenize(sentence.lower())
-# tagged_sentence = tagger.tag(tokens)
-# print("Tagged sentence:", tagged_sentence)
+    # #Model saver
+    with open("hmm_waray_tagger.pickle", "wb") as model_file:
+        dill.dump(tagger, model_file)
 
-# #Model saver
-with open("hmm_waray_tagger.pickle", "wb") as model_file:
-    dill.dump(tagger, model_file)
+if __name__ == "__main__":
+    main()
