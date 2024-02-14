f56ff2420947992b01663d22fb2c72eb435d35a6
Shirozo <caranzoneilbryan@gmail.com>
2024-02-14 21:12:37 +0800

    Checks whether a number or a special character

---
+++ parser/checker.py
@@ -0,0 +1,12 @@
+import re
+
+def is_special(string):
+	pattern = re.compile(r"^[^\w\s]+$")
+	return bool(re.match(pattern, string))
+
+def is_int(param):
+    try:
+        int(param)
+        return True
+    except:
+        return False
