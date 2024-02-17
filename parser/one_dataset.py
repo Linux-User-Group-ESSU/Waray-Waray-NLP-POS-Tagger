ab38bbfb880a258c93e640ff366bb398a7c9a08f
Shirozo <caranzoneilbryan@gmail.com>
2024-02-17 21:39:58 +0800

    Function to compile all dataset into 1 file

---
+++ parser/one_dataset.py
@@ -0,0 +1,16 @@
+def compile_dataset(foldername : str, output_folder : str) -> None:
+    
+    from read_files import read_files
+
+    dataset = read_files(foldername)
+    
+    with open(f"{output_folder}/all.csv", "w") as output_file:
+
+        for i in dataset:
+            with open(f"{foldername}/{i}", "r") as input_file:
+                input_reader = input_file.readlines()
+                for line in input_reader:
+                    output_file.write(line)
+
+if __name__ == "__main__":
+    compile_dataset("../Tagged_Article", "../Tagged_Article/Compiled_Dataset")
