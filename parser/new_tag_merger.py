ce3e64ace7acb59303f4955f0fd0fd76ea6cabe2
Shirozo <caranzoneilbryan@gmail.com>
2024-02-22 17:25:30 +0800

    Function used to update the tags

---
+++ parser/new_tag_merger.py
@@ -0,0 +1,11 @@
+import csv
+
+with open("dataset.csv", "r") as new_tags:
+    with open("dataset1.csv", "a") as old_tags:
+        reader = csv.reader(new_tags)
+
+        for i in reader:
+            if i[0] and i[1]:
+                word = i[0].strip()
+                tag = i[1].replace('\n', '').strip()
+                old_tags.writelines(f"{word}|{tag}\n")
