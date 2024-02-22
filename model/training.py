f5ddce8facb2e9d13d3fe93501aa44464e116957
Shirozo <caranzoneilbryan@gmail.com>
2024-02-22 22:10:12 +0800

    Added a smoothing for runtime overflow

--- model/training.py
+++ model/training.py
@@ -1,13 +1,13 @@
 import csv
-import nltk
+from  nltk.probability import LaplaceProbDist
 from nltk.tag import hmm
 import dill
-import os
 from tqdm import tqdm
 
 def load_dataset_from_csv(csv_file):
     #Placeholder to contain all the training sentence and tags
     tagged_data = []
+    states = []
     with open(csv_file, 'r') as file:
         #Read the dataset
         reader = csv.reader(file)
@@ -16,9 +16,10 @@ def load_dataset_from_csv(csv_file):
         for row in reader:
             for i in range(len(row)):
                 data = row[i].split("|")
+                if data[1].strip() not in states : states.append(data[1].strip())
                 sentence_tags.append((data[0].strip().lower(), data[1].strip()))
             tagged_data.append(sentence_tags)
-    return tagged_data
+    return tagged_data, states
 
 
 def split_dataset(dataset, split=.9):
@@ -26,32 +27,28 @@ def split_dataset(dataset, split=.9):
     split_index = int(len(dataset) * split)
     return dataset[:split_index], dataset[split_index:]
 
-def train_hmm(dataset):
-    trainer = hmm.HiddenMarkovModelTrainer()
+def train_hmm(dataset, states):
+    trainer = hmm.HiddenMarkovModelTrainer(
+        states=states,
+    )
     for i in tqdm(range(1), desc="Training"):
-        tagger = trainer.train_supervised(dataset)
+        #Uses the LapLace
+        tagger = trainer.train_supervised(dataset, estimator=lambda fd, bins: LaplaceProbDist(fd, bins))
     return tagger
 
-def main():    
-    train_data = load_dataset_from_csv(f"../Tagged_Article/Compiled_Dataset/all.csv")
+def main():   
+    train_data, states = load_dataset_from_csv(f"../Tagged_Article/Compiled_Dataset/all.csv")
+    print(states)
     #Split the dataset
     train, test = split_dataset(train_data)
     # #Train
-    tagger = train_hmm(train)
+    tagger = train_hmm(train, states)
 
-        # #Accuracy tester
-    acurracy = tagger.accuracy(test)
-    print(f"Accuracy: {acurracy}")
+    print(f"Accuracy: {tagger.accuracy(test)}")
 
-    #Test
-    # sentence = "usa, duha"
-    # tokens = ["usa", "duha"]
-    # tagged_sentence = tagger.tag(tokens)
-    # print("Tagged sentence:", tagged_sentence)
-
-    # #Model saver
     with open("hmm_waray_tagger.pickle", "wb") as model_file:
         dill.dump(tagger, model_file)
+
 
 if __name__ == "__main__":
     main()
