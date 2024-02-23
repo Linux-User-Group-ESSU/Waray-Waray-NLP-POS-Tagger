dc4e124dc296b9700be2701b8312fc46d013d1c8
Shirozo <caranzoneilbryan@gmail.com>
2024-02-23 21:12:14 +0800

    Append to write

--- parser_nlp/saver.py
+++ parser_nlp/saver.py
@@ -1,6 +1,7 @@
 def save_unknown_tags(filepath : str, untagged : list[str], untagged_in_file : list[str]) -> None:
     import csv
 
+    #Changes to write mode to see how many tags are untagged
     with open(filepath, "w") as untagged_file:
         untagged_writer = csv.writer(untagged_file)
         for i in untagged:
