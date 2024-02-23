08d31b1e10a0fbd0d03875ab9a78c4ce4757eb92
Shirozo <caranzoneilbryan@gmail.com>
2024-02-23 21:11:48 +0800

    Append to write

--- parser_nlp/saver.py
+++ parser_nlp/saver.py
@@ -1,7 +1,7 @@
 def save_unknown_tags(filepath : str, untagged : list[str], untagged_in_file : list[str]) -> None:
     import csv
 
-    with open(filepath, "a") as untagged_file:
+    with open(filepath, "w") as untagged_file:
         untagged_writer = csv.writer(untagged_file)
         for i in untagged:
             if  i not in untagged_in_file:
