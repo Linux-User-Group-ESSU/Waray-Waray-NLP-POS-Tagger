f25fab9f9016eaf61684a5412f95d9b223df7394
Shirozo <caranzoneilbryan@gmail.com>
2024-02-24 21:05:45 +0800

    Fix bug index out of range

--- parser_nlp/spliiter.py
+++ parser_nlp/spliiter.py
@@ -1,4 +1,4 @@
 def split_dataset(dataset, split=.9):
     # * 90% training set and 10% test set
     split_index = int(len(dataset) * split)
-    return dataset[:split_index], dataset[split_index:]
+    return dataset[split_index:], dataset[:split_index]
