507f426d621115cc288614f933e1d7042285a9b2
Shirozo <caranzoneilbryan@gmail.com>
2024-02-26 09:25:37 +0800

    New graph

---
+++ wara-tagger/JS/style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export default style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20
+      },
+      input_style : {
+        padding: 10,
+        backgroundColor: "#F9F9F9",
+        width: "100%",
+        height: "40%",
+        borderRadius: 10,
+        overflow: "scroll"
+      },
+      taggedWord: {
+        height: "40%",
+        width: "100%",
+        backgroundColor: '#c2c1c0',
+      }
+})
