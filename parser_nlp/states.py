ab4aa08047b74774440ec9babd8d3fd73a0ce349
Shirozo <caranzoneilbryan@gmail.com>
2024-03-01 16:54:34 +0800

    XX -> OOV'
    '

--- parser_nlp/states.py
+++ parser_nlp/states.py
@@ -1,1 +1,1 @@
-STATES = ['VB', 'XX', 'NMBR', '?', 'CLTC', 'DT', 'LNKR', 'PNN', 'NN', 'MDFR', 'CJTN', 'IJTN', 'AFX', 'RP', 'IDEX', 'PSTN', 'ADJV', 'ADVB', 'NTRG']
+STATES = ['VB', 'OOV', 'NMBR', '?', 'CLTC', 'DT', 'LNKR', 'PNN', 'NN', 'MDFR', 'CJTN', 'IJTN', 'AFX', 'RP', 'IDEX', 'PSTN', 'ADJV', 'ADVB', 'NTRG']
