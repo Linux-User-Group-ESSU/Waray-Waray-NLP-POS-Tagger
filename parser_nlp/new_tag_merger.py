70a87527ff069c77a8a43e59b56f8c67d2f5ae35
Shirozo <caranzoneilbryan@gmail.com>
2024-03-01 16:52:44 +0800

    Change the implementation of the recent tag csv structure

--- parser_nlp/new_tag_merger.py
+++ parser_nlp/new_tag_merger.py
@@ -1,11 +1,18 @@
 import csv
+from checker import is_special
 
-with open("dataset.csv", "r") as new_tags:
+with open("../Untagged_Word/untagged.csv", "r") as new_tags:
     with open("newTag.csv", "a") as old_tags:
-        reader = csv.reader(new_tags)
+        for i in new_tags.readlines():
+            word = i.split("|")[0]
+            tag = i.split("|")[1].replace("\n", "").strip()
+            old_tags.writelines(f"{word}|{tag}\n")
 
-        for i in reader:
-            if i[0] and i[1]:
-                word = i[0].strip()
-                tag = i[1].replace('\n', '').strip()
-                old_tags.writelines(f"{word}|{tag}\n")
+        # for i in reader:
+        #     for j in i:
+        #         if j:
+        #             word = i.split("|")[0]
+        #             if word not in datas and not is_special(word) and not word.isdigit():
+        #                 datas.append(word)
+        #                 tag = j.split("|")[1]
+        #                 old_tags.writelines(f"{word}|{tag}\n")
