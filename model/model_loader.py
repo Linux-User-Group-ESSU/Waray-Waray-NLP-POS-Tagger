aec746c8487a61775468729f0edf32bf0b952e17
Shirozo <caranzoneilbryan@gmail.com>
2024-03-08 20:02:10 +0800

    Updated

--- model/model_loader.py
+++ model/model_loader.py
@@ -2,13 +2,14 @@ import dill
 from tokenizer import tokenize_text
 import os
 
-#Buksa an model nga ginsave
+#Kuhaa an absolute path han model
 model_path = os.path.join(os.path.dirname(__file__), "model.pickle")
+
+#Buksa an model nga ginsave
 with open(model_path, "rb") as tag:
 
     #Ig load an binary nga model into python
     tagger = dill.load(tag)
-    print(tagger)
 
     #Word ngaim karuyag i-tag
     word = "it mga maestro ha essu sige la it pamolmol hin nakadto ha cebu"
