7ecf797365dde72d5984626d034e663344a8be98
nellborj <nellborj78@gmail.com>
2024-03-10 09:17:53 +0800

    Removed Unused Tags

--- parser_nlp/states.py
+++ parser_nlp/states.py
@@ -1,1 +1,2 @@
-STATES = ['VB', 'OOV', 'NMBR', '?', 'CLTC', 'DT', 'LNKR', 'PNN', 'NN', 'MDFR', 'CJTN', 'IJTN', 'AFX', 'RP', 'IDEX', 'PSTN', 'ADJV', 'ADVB', 'NTRG', 'IJTC']
+STATES = ['VB', 'OOV', 'NMBR', '?', 'CLTC', 'DT', 'LNKR', 'PNN', 'NN', 'MDFR', 'CJTN', 'IJTN', 'RP', 'PSTN', 'NTRG']
+#AFX, IDEX, ADVB, ADJV
