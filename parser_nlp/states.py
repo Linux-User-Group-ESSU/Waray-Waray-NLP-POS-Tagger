4b323fe3aeceac80273f3cc96b02c622151b71fb
nellborj <nellborj78@gmail.com>
2024-03-10 11:30:15 +0800

    Removed Unused Tags

--- parser_nlp/states.py
+++ parser_nlp/states.py
@@ -1,2 +1,6 @@
+<<<<<<< Updated upstream
 STATES = ['VB', 'OOV', 'NMBR', '?', 'CLTC', 'DT', 'LNKR', 'PNN', 'NN', 'MDFR', 'CJTN', 'IJTN', 'RP', 'PSTN', 'NTRG']
+=======
+STATES = ['VB', 'OOV', 'NMBR', '?', 'CLTC', 'DT', 'LNKR', 'PNN', 'NN', 'MDFR', 'CJTN', 'IJTN', 'RP', 'IDEX', 'PSTN', 'NTRG']
+>>>>>>> Stashed changes
 #AFX, IDEX, ADVB, ADJV
