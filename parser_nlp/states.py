16d4fac330b3bb1e4bcc09d22f5f53e7d1ec70d8
Shirozo <caranzoneilbryan@gmail.com>
2024-03-18 22:24:17 +0800

    Fix merge

--- parser_nlp/states.py
+++ parser_nlp/states.py
@@ -1,6 +1,1 @@
-<<<<<<< Updated upstream
 STATES = ['VB', 'OOV', 'NMBR', '?', 'CLTC', 'DT', 'LNKR', 'PNN', 'NN', 'MDFR', 'CJTN', 'IJTN', 'RP', 'PSTN', 'NTRG']
-=======
-STATES = ['VB', 'OOV', 'NMBR', '?', 'CLTC', 'DT', 'LNKR', 'PNN', 'NN', 'MDFR', 'CJTN', 'IJTN', 'RP', 'IDEX', 'PSTN', 'NTRG']
->>>>>>> Stashed changes
-#AFX, IDEX, ADVB, ADJV
