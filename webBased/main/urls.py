0f9e2c07fa7d89a6300b6aaabc2f90b86192a8a4
Shirozo <caranzoneilbryan@gmail.com>
2024-05-02 01:04:48 +0800

    API done

--- webBased/main/urls.py
+++ webBased/main/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", view=views.index, name="index"),
-    path("read/", view=views.read_file_content, name="read")
+    path("read/", view=views.read_file_content, name="read"),
+    path("download/", view=views.save_in_server, name="download"),
+    path("save/", view=views.save_in_server, name="save"),
+    path("readDb", view=views.readDb, name="readDb")
 ]
