ee3dc20c2359f6d29e8d6118bb8118e47dd2db97
Shirozo <caranzoneilbryan@gmail.com>
2024-05-05 00:52:01 +0800

    Added error checking if entry exist in db or file exist in server

--- webBased/main/models.py
+++ webBased/main/models.py
@@ -3,10 +3,17 @@ from django import forms
 import os
 
 # Create your models here.
-def validate_file(voter_file):
-    if voter_file.name.endswith(".csv") or voter_file.name.endswith(".txt"):
-        if os.path.exists(f"media/{voter_file}"):
-            raise forms.ValidationError("File already exist in the database!")
+def validate_file(dataset_file):
+    if dataset_file.name.endswith(".csv") or dataset_file.name.endswith(".txt"):
+        if str(dataset_file.file).endswith(".txt"):
+            dataset_file = str(dataset_file.file).replace(".txt", ".csv")
+        
+        if os.path.exists(f"media/{dataset_file}"):
+            raise forms.ValidationError("File already exist in the server!\nContact Admin!")
+        
+        has_data = FileUploaded.objects.filter(file = f"media/{dataset_file}")
+        if has_data.exists():
+            raise forms.ValidationError("File already exist in the database!\nContact Admin!")
     else:
         raise forms.ValidationError("Not a txt/csv file!")
 class FileUploaded(models.Model):
